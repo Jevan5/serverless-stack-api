98d4fdfe920a3ff9e020127873da4e7e56521877
Joshua Evans <joshevans.tk@gmail.com>
2020-05-22 17:52:58 -0400

    Re-triggering build

--- delete.js
+++ delete.js
@@ -1,6 +1,7 @@
 import handler from './libs/handler-lib';
 import dynamoDb from './libs/dynamodb-lib';
 
+
 export const main = handler(async (event, context) => {
     const params = {
         TableName: process.env.tableName,
