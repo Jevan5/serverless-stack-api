45ab9c5e353c05f22665d1e8e6147b4416ae1022
Joshua Evans <joshevans.tk@gmail.com>
2020-05-22 22:44:30 -0400

    Adding error logging

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,15 +1,20 @@
+import * as debug from './debug-lib';
+
 export default function handler(lambda) {
     return async function(event, context) {
         let statusCode;
         let body;
 
         try {
+            debug.init(event, context);
             body = await lambda(event, context);
             statusCode = 200;
         } catch (e) {
             body = { error: e.message };
             statusCode = 500;
         }
+
+        debug.end();
 
         return {
             statusCode: statusCode,
